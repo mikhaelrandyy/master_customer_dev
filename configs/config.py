d88caffa2c5fdf74646d8b869779c86b72c3bc87
ginanjar <ginanjar.sumantri@agungsedayu.com>
2025-03-13 11:16:03 +0700

    config

--- configs/config.py
+++ configs/config.py
@@ -2,12 +2,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
-    OAUTH2_URL: str = ""
-    OAUTH2_TOKEN: str = ""
+    OAUTH2_URL: str
+    OAUTH2_TOKEN: str
     DB_CONFIG: str
-    PROJECT_NAME: str = ""
-    UI_URL: str = ""
-    PUBSUB_SUFFIX: str = ""
+    UI_URL: str
+    PROJECT_NAME: str
+    PUBSUB_SUFFIX: str
     AZURE_TENANT_ID: str 
     AZURE_CLIENT_ID: str
     AZURE_CLIENT_SECRET: str
